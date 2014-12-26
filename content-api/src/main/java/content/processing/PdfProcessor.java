700d0463e48d1357fcbbf6e77b480942736c7360
Rikard Holm <rikardholm@gmail.com>
2014-12-26 12:31:44 +0100

    Removed old itext processor.

--- content-api/src/main/java/content/processing/PdfProcessor.java
+++
@@ -1,9 +0,0 @@
-package content.processing;
-
-import java.util.Map;
-
-public interface PdfProcessor {
-    ProcessingResult process(String templatePath, Map<String, Object> model);
-
-    Session session(String templatePath);
-}
