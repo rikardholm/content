7fb244768f0a05721afbca030245a43ad5a1c5b8
Rikard Holm <rikardholm@gmail.com>
2014-12-29 23:42:09 +0100

    Spring RestTemplate implementation

--- content-engine/src/main/java/content/processing/ProcessorFactory.java
+++ content-engine/src/main/java/content/processing/ProcessorFactory.java
@@ -2,34 +2,26 @@ package content.processing;
 
 import content.processing.internal.TemplateProvider;
 import content.processing.internal.pdf.ITextProcessor;
-import content.processing.internal.provisioning.HttpTemplateProvider;
-import content.processing.internal.provisioning.Transform;
+import content.processing.internal.provisioning.SpringRestTemplateProvider;
 import content.processing.internal.text.JmteProcessor;
 
-import javax.ws.rs.core.Response;
-import java.io.InputStream;
 import java.util.Objects;
-import java.util.function.Function;
 
 public class ProcessorFactory {
 
     public static Processor<byte[]> createPdfProcessor(String serverConnection) {
         Objects.requireNonNull(serverConnection);
-        TemplateProvider<byte[]> templateProvider = httpTemplateProvider(serverConnection, readEntity(InputStream.class).andThen(Transform::toByteArray));
+        TemplateProvider<byte[]> templateProvider = httpTemplateProvider(serverConnection, byte[].class);
         return new ITextProcessor(templateProvider);
     }
 
     public static Processor<String> createTextProcessor(String serverConnection) {
         Objects.requireNonNull(serverConnection);
-        TemplateProvider<String> templateProvider = httpTemplateProvider(serverConnection, readEntity(String.class));
+        TemplateProvider<String> templateProvider = httpTemplateProvider(serverConnection, String.class);
         return new JmteProcessor(templateProvider);
     }
 
-    private static <T> TemplateProvider<T> httpTemplateProvider(String serverConnection, Function<Response, T> transformer) {
-        return new HttpTemplateProvider<>(serverConnection, "templates", transformer);
-    }
-
-    private static <T> Function<Response, T> readEntity(Class<T> type) {
-        return response -> response.readEntity(type);
+    private static <T> TemplateProvider<T> httpTemplateProvider(String serverConnection, Class<T> type) {
+        return new SpringRestTemplateProvider<>(serverConnection, "templates/", type);
     }
 }
---
+++ content-engine/src/main/java/content/processing/internal/provisioning/SpringRestTemplateProvider.java
@@ -0,0 +1,30 @@
+package content.processing.internal.provisioning;
+
+import content.processing.TemplateProvisioningException;
+import content.processing.internal.Template;
+import content.processing.internal.TemplateProvider;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+public class SpringRestTemplateProvider<CONTENT> implements TemplateProvider<CONTENT> {
+
+    private final String rootUrl;
+    private final Class<CONTENT> type;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public SpringRestTemplateProvider(String serverConnection, String rootPath, Class<CONTENT> type) {
+        this.rootUrl = serverConnection + rootPath;
+        this.type = type;
+    }
+
+    @Override
+    public Template<CONTENT> get(String path) {
+        CONTENT response;
+        try {
+            response = restTemplate.getForObject(rootUrl + path, type);
+        } catch (RestClientException e) {
+            throw new TemplateProvisioningException(e);
+        }
+        return new Template<>(response);
+    }
+}
